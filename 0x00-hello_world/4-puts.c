3372bd40b225c34cfeb2bded9d3a129ac4b4acd6
Igbineweka <kelvin.igbineweka@yahoo.com>
2023-08-10 20:29:43 +0100

    hello_puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - programming to
+ * demostrate puts usage
+ * Return: always 0
+ */
+
+int main(void)
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+
+}
