af75334bd38fe66635c15667b54dd4f1ef25630f
Igbineweka <kelvin.igbineweka@yahoo.com>
2023-08-10 20:43:16 +0100

    hello_printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - code to desmostrate
+ * printf usage
+ * Return: always 0
+ */
+
+int main(void)
+
+{
+
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+
+}
