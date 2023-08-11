b8f8c4b6f4c06c59f7cfa27a6818ae786710619a
Igbineweka <kelvin.igbineweka@yahoo.com>
2023-08-11 18:30:33 +0100

    combination of single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - program for possible combine
+ * combination of single digits number
+ *Return: always 0
+ */
+
+int main(void)
+
+{
+/* declare variable for single digit number */
+
+char (c);
+	for (c = '1'; c <= '9'; c++)
+	{
+	putchar(c);
+		if (c != '9')
+		{
+		putchar(',');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+
+}
