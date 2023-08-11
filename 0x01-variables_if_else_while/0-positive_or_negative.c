0f8384ddc0428eb1aaf71cc2c26225db70ca2ea9
Igbineweka <kelvin.igbineweka@yahoo.com>
2023-08-11 10:12:05 +0100

    asigning_random_numbers

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program to asign random
+ * number to n each time executed
+ * Return: always 0
+ */
+
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if (n > 0)
+{
+	printf("is positive\n");
+	}
+if (n == 0)
+{
+	printf("is zero\n");
+	}
+if (n < 0)
+{
+	printf("is negative\n");
+	}
+return (0);
+}
