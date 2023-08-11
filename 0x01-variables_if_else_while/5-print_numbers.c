d9dfd3436442f03adc3360a3456e448663a3e363
Igbineweka <kelvin.igbineweka@yahoo.com>
2023-08-11 15:40:42 +0100

    print_numbers_of_base_10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - program that print all
+ * single digits numbers of base 10
+ * starting from 0
+ * Return: always 0
+ */
+
+int main(void)
+{
+/* declare variable for numbers in base 10 */
+
+int a;
+
+for (a = 0; a <= 9; a++)
+	{
+	printf("%d", a);
+	}
+	printf("\n");
+
+return (0);
+
+}
