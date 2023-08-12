c1e9d6206c9728fb7f2af5ba30e6fc7d239a32d8
Igbineweka <kelvin.igbineweka@yahoo.com>
2023-08-12 06:50:14 +0100

    printing number comninations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - program for number combination
+ * without repetition
+ * Return: always 0
+ */
+
+int main(void)
+
+{
+	int u, v; /* declare variable u and v */
+
+		for (u = 0; u < 9; u++)
+		{
+			for (v = u + 1; v < 10; v++)
+			{
+			putchar(u + '0');
+			putchar(v + '0');
+			if (u != 8) /* don't print comma after last number */
+				{
+				putchar(' ');
+				putchar(',');
+				}
+			}
+		}
+	putchar('\n');
+return (0);
+
+}
