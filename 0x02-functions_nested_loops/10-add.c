532ef59411c95037acfaf1db4ec4d501541a7a47
Igbineweka <kelvin.igbineweka@yahoo.com>
2023-08-15 22:12:55 +0100

    add integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers
+ * @n1: first integer
+ * @n2: second integer
+ * Return: result
+ */
+
+int add(int n1, int n2)
+
+{
+	return (n1 + n2);
+}
