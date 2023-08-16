f68fc7f3e8acfa5c7912d88c6df63212dc3f26ed
Igbineweka <kelvin.igbineweka@yahoo.com>
2023-08-16 03:57:56 +0100

    first 50 Fibonacci numbers, starting with 1 and 2

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main -  program that prints the first 50
+ * Fibonacci numbers, starting with 1 and 2,
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int c;
+	unsigned long f1 = 0, f2 = 1, sum;
+
+	for (c = 0; c < 50; c++)
+	{
+		sum = f1 + f2;
+		printf("%lu", sum);
+
+		f1 = f2;
+		f2 = sum;
+
+		if (c == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
