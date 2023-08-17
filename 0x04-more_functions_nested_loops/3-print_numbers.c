4285808c18cb2eacc6d8d4870c0bc74d6c4dac2b
Igbineweka <kelvin.igbineweka@yahoo.com>
2023-08-17 06:17:09 +0100

    print digits

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the numbers, from 0 to 9,
+ * followed by a new line.
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
