ead23d766ee6a385a8ee8d3b871a5f7e4dafdfc1
Igbineweka <kelvin.igbineweka@yahoo.com>
2023-08-21 19:01:47 +0100

    copying string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that copies the string pointed to
+ * by src, including the terminating null byte (\0),
+ * to the buffer pointed to by dest.
+ * @dest: destination of string
+ * @src: source of string
+ * Return: pointer to dest.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *p = dest;
+
+	while (*src)
+		*dest++ = *src++;
+	return (p);
+}
+
