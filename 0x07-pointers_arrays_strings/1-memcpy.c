74bfc0183d53348de1c0dc107bd1b07d098bf6a8
Igbineweka <kelvin.igbineweka@yahoo.com>
2023-08-28 14:55:47 +0100

    function that copies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,6 +5,7 @@
  * @dest: location string to copy to
  * @src: location to copy from
  * Return: pointer to dest.
+ * @n: number of bytes
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
