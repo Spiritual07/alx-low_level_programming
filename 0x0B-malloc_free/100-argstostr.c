5563ea218e4b4fa60f83bb31c1eb4dc5fd9fe04f
Igbineweka <kelvin.igbineweka@yahoo.com>
2023-09-05 10:04:32 +0100

    concatenates all the arguments of your program.

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *argstostr - function that concatenates all the arguments of your program.
+ * @ac: argument count
+ * @av: argument vector
+ * Return: Returns a pointer to a new string, or NULL if it fails
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int x, y;
+	int z = 0;
+	int len = 0;
+	char *str;
+
+/* calculate length of string to be concatenated */
+	for (x = 0; x < ac; x++)
+	{
+		if (av[x] == NULL) /* check if av is NULL */
+		{
+			return (NULL);
+		}
+		for (y = 0; av[x][y] != '\0'; y++)
+		{
+			len++;
+		}
+		len++; /* account for null terminator at the end of string */
+	}
+	/* allocate memory to the concatenated string */
+	str = malloc(sizeof(char) * (len + 1));
+	/* check if memory allocation failed */
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	/* copy characters from av to str */
+	for (x = z = 0; x < ac; x++, z++)
+	{
+		for (y = 0; av[x][y] != '\0'; y++)
+		{
+			str[z] = av[x][y];
+			z++;
+		}
+		str[z] = '\n'; /* add newline character after each string */
+	}
+	str[z] = '\0'; /* add null terminator */
+	return (str); /* return pointer to new string */
+}
