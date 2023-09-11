b3fd1d499166ebaeedb874c1f6ce6972184e799a
Igbineweka <kelvin.igbineweka@yahoo.com>
2023-09-11 11:59:42 +0100

    struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
-	char* name;
+/**
+ * struct dog - structure to keep record of a dog.
+ * @name: name of dog
+ * @age: dog age
+ * @owner: owner of dog
+ */
+
+struct dog
+{
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
 
 
