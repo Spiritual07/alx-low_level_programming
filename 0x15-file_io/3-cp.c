2284c4e87673941df431377aacfcaf9e5df10452
Igbineweka <kelvin.igbineweka@yahoo.com>
2023-10-02 16:29:08 +0100

    copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,36 +15,36 @@ int main(int ac, char *av[])
 
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	}
 	file_from = open(av[1], O_RDONLY);
 	if (file_from < 0)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	}
 	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to < 0)
 	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
 	while ((read_size = read(file_from, buffer, 1024)) > 0)
 	{
 		if (write(file_to, buffer, read_size) != read_size)
 		{
-			dprintf(2, "Error: Can't write to %s\n", av[2]), exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 		}
 	}
 	if (read_size < 0)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	}
 	if (close(file_from) < 0)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", file_from), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
 	}
 	if (close(file_to) < 0)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", file_to), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
 	}
 
 	return (0);
